2aacc7ab089d96937e0b44997c13aaaee8344ad2
kwan2 <jgh9987@naver.com>
2021-11-14 06:58:37 +0900

    로그인 완료

--- developEnvironment/basicFrame11_11/beans/User/userDAO.java
+++ developEnvironment/basicFrame11_11/beans/User/userDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class userDAO {
 	private Connection conn;
@@ -12,17 +13,17 @@ public class userDAO {
 	
 	public userDAO(){
 		try{
-			String dbURL ="jdbc:mysql:/'localhost:3306/webdb";
-			String dbID =""; //db아이디 
-			String dbPassword ="";//db비밀번호
-			Class.forName("com.mysql.jdb.Driver");
+			String dbURL ="jdbc:mysql://localhost:3306/webdb?useSSL=false";
+			String dbID ="root"; //db아이디 
+			String dbPassword ="root";//db비밀번호
+			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 	public int login(String userID, String userPassword){
-		String SQL ="SELECT userPassword FROM WHERE usrID =? ";
+		String SQL ="SELECT userPassword FROM user WHERE userID=?";
 		try{
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, userID);
@@ -37,23 +38,24 @@ public class userDAO {
 			return -1; // ID가 없음
 		}catch(Exception e){
 			e.printStackTrace();
+		}finally{
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
 		}
 		return -2; // db 오류
-	}
-	public int registerInfo(userDTO user){
-		String SQL = "INSERT INTO USER VALUES (?,?,?,?,?,?)";
-		try{
-			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1, user.getUserID());
-			pstmt.setString(2, user.getUserPassword());
-			pstmt.setString(3, user.getUserName());
-			pstmt.setString(4, user.getUserEmail());
-			pstmt.setString(5, user.getUserGender());
-			pstmt.setString(6, user.getUserNickname());
-			return pstmt.executeUpdate();
-		}catch(Exception e){
-			e.printStackTrace();
-		} 	
-		return -1; // db 오류
 	}
 }
