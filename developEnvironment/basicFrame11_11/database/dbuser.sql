550aa9afd77ae4cab1271f1196dc4d17e0386210
kwan2 <jgh9987@naver.com>
2021-11-15 20:42:40 +0900

    mysql->mariadb

--- developEnvironment/basicFrame11_11/database/dbuser.sql
+++ developEnvironment/basicFrame11_11/database/dbuser.sql
@@ -1,46 +1,19 @@
--- MySQL dump 10.13  Distrib 5.7.35, for Win64 (x86_64)
+-- MariaDB dump 10.19  Distrib 10.5.13-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: webdb
 -- ------------------------------------------------------
--- Server version	5.7.35-log
+-- Server version	10.5.13-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `post`
---
-
-DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `post` (
-  `postName` varchar(30) DEFAULT NULL,
-  `postDate` varchar(20) DEFAULT NULL,
-  `postInfo` varchar(50) DEFAULT NULL,
-  `exchangeArea` varchar(20) DEFAULT NULL,
-  `hashTag` varchar(40) DEFAULT NULL,
-  `userID` varchar(20) NOT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `post`
---
-
-LOCK TABLES `post` WRITE;
-/*!40000 ALTER TABLE `post` DISABLE KEYS */;
-/*!40000 ALTER TABLE `post` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -54,10 +27,9 @@ CREATE TABLE `user` (
   `userPassword` varchar(20) DEFAULT NULL,
   `userName` varchar(20) DEFAULT NULL,
   `userEmail` varchar(20) DEFAULT NULL,
-  `userGender` varchar(20) NOT NULL,
-  `userNickname` varchar(20) DEFAULT NULL,
+  `userGender` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-14  0:51:43
+-- Dump completed on 2021-11-15 20:35:54
