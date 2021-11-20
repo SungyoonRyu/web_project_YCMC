c09180f4837f0190bbeb47babc9f263ef608bda3
kwan2 <jgh9987@naver.com>
2021-11-20 17:28:06 +0900

    board 테이블 포함된 sql dump 파일

---
+++ developEnvironment/basicFrame11_18/21_11_20.sql
@@ -0,0 +1,85 @@
+-- MariaDB dump 10.19  Distrib 10.5.13-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: webdb
+-- ------------------------------------------------------
+-- Server version	10.5.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `board`
+--
+
+DROP TABLE IF EXISTS `board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `board` (
+  `boardID` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `area` varchar(20) NOT NULL,
+  `boardDate` datetime NOT NULL,
+  `boardContent` varchar(2050) NOT NULL,
+  `rentalDate` datetime NOT NULL,
+  `rentalCount` int(11) NOT NULL,
+  `userNickname` varchar(20) NOT NULL,
+  PRIMARY KEY (`boardID`),
+  KEY `userNickname` (`userNickname`),
+  CONSTRAINT `board_ibfk_1` FOREIGN KEY (`userNickname`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `board`
+--
+
+LOCK TABLES `board` WRITE;
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userID` varchar(20) NOT NULL,
+  `userPassword` varchar(20) DEFAULT NULL,
+  `userEmail` varchar(20) NOT NULL,
+  `userGender` varchar(20) DEFAULT NULL,
+  `userNickname` varchar(20) NOT NULL,
+  PRIMARY KEY (`userID`,`userNickname`,`userEmail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('1234','q','sdfsd@naver.com','male','sa'),('gildong11','','31312321@naver.com','male','saaa');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-20 17:26:52
