850b66c9e6c17b0571a3dc71649ba94333aa3456
kwan2 <jgh9987@naver.com>
2021-11-25 20:19:13 +0900

    어제한거대충 넣음

---
+++ developEnvironment/kwan/11-24/bean/comment/CommentDTO.java
@@ -0,0 +1,35 @@
+package comment;
+
+import java.util.Date;
+
+public class CommentDTO {
+	private String boardID;
+	private String commentUser;
+	private String commentDate;
+	private String CommentContent;
+
+	public String getCommentUser() {
+		return commentUser;
+	}
+	public String getBoardID() {
+		return boardID;
+	}
+	public void setBoardID(String boardID) {
+		this.boardID = boardID;
+	}
+	public void setCommentUser(String commentUser) {
+		this.commentUser = commentUser;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String nowday) {
+		this.commentDate = nowday;
+	}
+	public String getCommentContent() {
+		return CommentContent;
+	}
+	public void setCommentContent(String commentContent) {
+		CommentContent = commentContent;
+	}
+}
