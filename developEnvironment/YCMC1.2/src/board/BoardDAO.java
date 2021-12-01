d853331d5b6c0be60c6a3460822722595ebbba7d
kwan2 <jgh9987@naver.com>
2021-12-02 00:00:29 +0900

    boardDAO edit

--- developEnvironment/YCMC1.2/src/board/BoardDAO.java
+++ developEnvironment/YCMC1.2/src/board/BoardDAO.java
@@ -18,7 +18,7 @@ public class BoardDAO {
 		try{
 			String dbURL ="jdbc:mariadb://localhost:3307/webdb?useSSL=false";
 			String dbID ="root"; //db아이디 
-			String dbPassword ="1234";//db비밀번호
+			String dbPassword ="root1234";//db비밀번호
 			Class.forName("org.mariadb.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
 		}catch(Exception e){
@@ -60,159 +60,46 @@ public class BoardDAO {
 		return false;
 	}
 	public ArrayList<BoardDTO> boardMainList(){
-		String SQL ="select * from board order by boardDate DESC"; 
-		int num = 0;
-		ArrayList <BoardDTO> boardArr = new ArrayList<BoardDTO>();
-	try{
-		pstmt = conn.prepareStatement(SQL);
-		rs = pstmt.executeQuery();
-		while(num<9 && rs.next()){
-			BoardDTO board = new BoardDTO();
-			board.setBoardID(rs.getString(1));
-			board.setTitle(rs.getString(2));			
-			board.setBoardDate(rs.getString(3));
-			board.setBoardContent(rs.getString(4));
-			board.setRentalDate1(rs.getString(5));
-			board.setRentalDate2(rs.getString(6));
-			board.setRentalCount(rs.getInt(7));
-			board.setUserNickname(rs.getString(8));
-			board.setBoardCategory(rs.getString(9));
-			boardArr.add(board);
-			num++;
-		}
-	}catch(Exception e){
-		e.printStackTrace();
-	}finally{
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			pstmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			rs.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	return boardArr;
-	}
-	public boolean boardModify(BoardDTO board){
-		String SQL ="UPDATE board SET title = ?, boardContent = ?,rentalDate2 = ?, rentalDate2 = ?, boardCategory = ? WHERE boardID = ?"; 	  
-		try{
-			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1,board.getTitle());
-			pstmt.setString(2,board.getBoardContent());
-			pstmt.setString(3,board.getRentalDate1());
-			pstmt.setString(4,board.getRentalDate2());
-			pstmt.setString(5, board.getBoardCategory());
-			pstmt.setString(6,board.getBoardID());
-			if(pstmt.executeUpdate() == 1){
-				return true;
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			try {
-				pstmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}return false; // db 오류 
-	}
-	public boolean boardDelete(String boardID){
-		String SQL ="DELETE FROM board where boardID = ?";
-		try{
-			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1,boardID);
-			if(pstmt.executeUpdate() == 1){
-				return true;
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			try {
-				pstmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return false; // db오류
-	}
-	public BoardDTO boardInstance(String boardID){
-		String SQL ="select * from board WHERE boardID = ?";
-		BoardDTO board = new BoardDTO();
-	try{
-		pstmt = conn.prepareStatement(SQL);
-		pstmt.setString(1,boardID);
-		rs = pstmt.executeQuery();
-		if(rs.next()){
-			board.setBoardID(rs.getString(1));
-			board.setTitle(rs.getString(2));
-			board.setBoardDate(rs.getString(3));
-			board.setBoardContent(rs.getString(4));
-			board.setRentalDate1(rs.getString(5));
-			board.setRentalDate2(rs.getString(6));
-			board.setRentalCount(rs.getInt(7));
-			board.setUserNickname(rs.getString(8));
-			board.setBoardCategory(rs.getString(9));
-		}
-	}catch(Exception e){
-		e.printStackTrace();
-	}finally{
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			pstmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			rs.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	return board;
-	}
-	public int modifyNickname(String newNickname,String oldNickname){
-        String SQL ="UPDATE board SET userNickname=? WHERE userNickname = ?";
-        try{
-            pstmt = conn.prepareStatement(SQL);
-            pstmt.setString(1,newNickname);
-            pstmt.setString(2,oldNickname);
-            return pstmt.executeUpdate();
-        }catch(Exception e){
+        String SQL ="select * from board order by boardDate DESC"; 
+        int num = 0;
+        ArrayList <BoardDTO> boardArr = new ArrayList<BoardDTO>();
+    try{
+        pstmt = conn.prepareStatement(SQL);
+        rs = pstmt.executeQuery();
+        while(num<9 && rs.next()){
+            BoardDTO board = new BoardDTO();
+            board.setBoardID(rs.getString(1));
+            board.setTitle(rs.getString(2));
+            board.setBoardDate(rs.getString(3));
+            board.setBoardContent(rs.getString(4));
+            board.setRentalDate1(rs.getString(5));
+            board.setRentalDate2(rs.getString(6));
+            board.setRentalCount(rs.getInt(7));
+            board.setUserNickname(rs.getString(8));
+            board.setBoardCategory(rs.getString(9));
+            boardArr.add(board);
+            num++;
+        }
+    }catch(Exception e){
+        e.printStackTrace();
+    }finally{
+        try {
+            conn.close();
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally{
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                pstmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }return -2; // db 오류 
+        }
+        try {
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    return boardArr;
     }
 	public ArrayList<BoardDTO> boardCategoryList(String boardCategory){
         String SQL ="select * from board where boardCategory =? order by boardDate DESC"; 
@@ -259,13 +146,13 @@ public class BoardDAO {
     }
 	public ArrayList<BoardDTO> boardSearchList(String title){
         String SQL ="select * from board WHERE title LIKE ?"; 
-    
+        int num = 0;
         ArrayList <BoardDTO> boardArr = new ArrayList<BoardDTO>();
     try{
         pstmt = conn.prepareStatement(SQL);
         pstmt.setString(1,"%"+title+"%");
         rs = pstmt.executeQuery();
-        while(rs.next()){
+        while(num<9 && rs.next()){
             BoardDTO board = new BoardDTO();
             board.setBoardID(rs.getString(1));
             board.setTitle(rs.getString(2));
@@ -277,6 +164,7 @@ public class BoardDAO {
             board.setUserNickname(rs.getString(8));
             board.setBoardCategory(rs.getString(9));
             boardArr.add(board);
+            num++;
         }
     }catch(Exception e){
         e.printStackTrace();
@@ -299,4 +187,125 @@ public class BoardDAO {
     }
     return boardArr;
     }
+	public boolean boardModify(BoardDTO board){
+		String SQL ="UPDATE board SET title = ?, boardContent = ?,rentalDate2 = ?, rentalDate2 = ?, rentalCount = ? WHERE boardID = ?"; 	  
+		try{
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,board.getTitle());
+			pstmt.setString(2,board.getBoardContent());
+			pstmt.setString(3,board.getRentalDate1());
+			pstmt.setString(4,board.getRentalDate2());
+			pstmt.setInt(5,board.getRentalCount());
+			pstmt.setString(6,board.getBoardID());
+			if(pstmt.executeUpdate() == 1){
+				return true;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}return false; // db 오류 
+	}
+	public boolean boardDelete(String boardID, String userNickname){
+		String SQL ="DELETE FROM board where boardID = ? and userNickname = ?";
+		try{
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,boardID);
+			pstmt.setString(2,userNickname);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				return true;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false; // db오류
+	}
+	public BoardDTO boardInstance(String boardID){
+        String SQL ="select * from board WHERE boardID = ?";
+        BoardDTO board = new BoardDTO();
+    try{
+        pstmt = conn.prepareStatement(SQL);
+        pstmt.setString(1,boardID);
+        rs = pstmt.executeQuery();
+        if(rs.next()){
+            board.setBoardID(rs.getString(1));
+            board.setTitle(rs.getString(2));
+            board.setBoardDate(rs.getString(3));
+            board.setBoardContent(rs.getString(4));
+            board.setRentalDate1(rs.getString(5));
+            board.setRentalDate2(rs.getString(6));
+            board.setRentalCount(rs.getInt(7));
+            board.setUserNickname(rs.getString(8));
+            board.setBoardCategory(rs.getString(9));
+        }
+    }catch(Exception e){
+        e.printStackTrace();
+    }finally{
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    return board;
+    }
+	public int modifyNickname(String newNickname,String oldNickname){
+		String SQL ="UPDATE board SET userNickname=? WHERE userNickname = ?"; 	  
+		try{
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,newNickname);
+			pstmt.setString(2,oldNickname);
+			return pstmt.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}return -2; // db 오류 
+	}
 }
+
